badd18a5dfb27100c19ff33d62b52a00a41291b6
MELIH AVCI - Dcs Cozumleri Mudurlugu <M_AVCI16@thynet.thy.com>
2026-01-21 11:54:35 +0300

    adjusted changes

--- CoreJava/src/main/java/CoreJava/a8_extras/Varargs.java
+++ CoreJava/src/main/java/CoreJava/a8_extras/Varargs.java
@@ -1,13 +1,13 @@
 package CoreJava.a8_extras;
 
-public class varargs {
+public class Varargs {
     void printNumbers(int... nums) {
         for (int num : nums) {
             System.out.println(num);
         }
     }
 public static void main(String[] args) {
-        varargs a = new varargs();
+        Varargs a = new Varargs();
         a.printNumbers(1, 2, 3); // 3 parametre
         a.printNumbers(4); // 1 parametre
     }
