cb14660f4ecb244f8d69dee357985e0ec350ffe8
MELIH AVCI - Dcs Cozumleri Mudurlugu <M_AVCI16@thynet.thy.com>
2026-01-29 14:49:40 +0300

    Added try with resources and finally implementations

--- DCS2.0/src/main/java/CoreJava/a7_exceptions/CheckedException.java
+++ DCS2.0/src/main/java/CoreJava/a7_exceptions/CheckedException.java
@@ -1,15 +1,32 @@
 package CoreJava.a7_exceptions;
-
-// Checked Exception Örneği
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 class Main {
     public static void main(String[] args) {
+        File file = new File("example.txt");
+        FileReader reader = null;
+
         try {
-            File file = new File("example.txt");
-            FileReader reader = new FileReader(file);
+            reader = new FileReader(file);
+            // Dosya okunurken işlemler...
+            System.out.println("Dosya okunuyor...");
+        } catch (IOException e) {
+            System.out.println("Dosya okunurken hata oluştu: " + e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println("Dosya okuyucu kapatılırken hata oluştu: " + e.getMessage());
+                }
+            }
+        }
+
+        try (FileReader reader2 = new FileReader(file)) {
+            // Dosya okunurken işlemler...
+            System.out.println("Dosya okunuyor...");
         } catch (IOException e) {
             System.out.println("Dosya okunurken hata oluştu: " + e.getMessage());
         }
