ec935c633fe17fd9798ca13f7293cf57df5e54ef
MELIH AVCI - Dcs Cozumleri Mudurlugu <M_AVCI16@thynet.thy.com>
2026-01-30 14:21:55 +0300

    Seperate branch for step1-a

--- DCS2.0/ModuleA/src/main/java/com/example/ModuleA/ExampleClassA.java
+++ DCS2.0/ModuleA/src/main/java/com/example/ModuleA/ExampleClassA.java
@@ -1,7 +1,12 @@
 package com.example.ModuleA;
 
-public class ÖrnekSınıf {
+public class ExampleClassA {
     public void sayHello() {
         System.out.println("Merhaba! ModuleA'dan bir sınıfım.");
     }
+    public void sayGoodbye() {
+        System.out.println("Görüşürüz!.");
+    }
 }
+
+
