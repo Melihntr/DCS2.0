4dae76fbbe4f54b3f4366949120e23c98122153d
Melihntr <melihavci@yahoo.com>
2026-02-05 10:00:12 +0300

    init

--- DCS2.0/src/main/java/CoreJava/a1_ecosystem/JavaVersionCheck.java
+++
@@ -1,8 +0,0 @@
-package CoreJava.a1_ecosystem;
-
-public class JavaVersionCheck {
-    public static void main(String args[]){
-        System.out.println(System.getProperty("java.version"));
-        System.out.println(System.getProperty("java.vm.name"));
-    }
-}
--- DCS2.0/src/main/java/CoreJava/a2_language_basics/operators/Ternary.java
+++
@@ -1,9 +0,0 @@
-package CoreJava.a2_language_basics.operators;
-
-public class Ternary {
-    public static void main(String[] args) {
-        int sayi = 10;
-        String sonuc = (sayi % 2 == 0) ? "Çift" : "Tek";
-        System.out.println(sonuc); // Output: Çift
-    }
-}
